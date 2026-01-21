42b22f8e85920b697175a53b8cf2dc6a90072f56
miahyeas109-create <miahyeas109@gmail.com>
2026-01-21 11:26:13 +0000

    fixed code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,6 @@
-The 'Message Seller' button says 'Coming Soon'. For now, please make it functional so that when clicked, it opens the user's phone dialer with the seller's number, or creates a simple chat window."import { Switch, Route } from "wouter";
+import { Switch, Route } from "wouter";
+
+
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
